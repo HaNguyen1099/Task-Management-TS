6ea704fb410d788411b335049b4839533ae9af27
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-18 21:39:24 +0700

    Tính năng tìm kiếm

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,11 +2,13 @@ import { Request, Response } from "express"
 import Task from "../models/task.model"
 
 import paginationHelper from "../../../helpers/pagination"
+import searchHelper from "../../../helpers/search"
 
 export const index = async (req: Request, res: Response) => {
     interface find {
         deleted: boolean,
-        status?: string
+        status?: string,
+        title?: RegExp
     }
 
     const find: find = {
@@ -24,6 +26,13 @@ export const index = async (req: Request, res: Response) => {
     if (req.query.sortKey && req.query.sortValue) {
         const sortKey = req.query.sortKey.toLocaleString()
         sort[sortKey] = req.query.sortValue
+    }
+
+    // Search 
+    let objectSearch = searchHelper(req.query)
+
+    if (req.query.keyword) {
+        find.title = objectSearch.regex 
     }
 
     // Pagination 
